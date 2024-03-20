3369921f41ff8dd3e6644911caa1bfabe1766907
ge54guv <ge54guv@mytum.de>
2024-03-20 11:44:48 +0100

    ns button changes its color

--- ns-backend.py
+++ ns-backend.py
@@ -1,17 +1,21 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
+from time import sleep
 from gpiozero import Button
 from threading import Thread
 
 app = Flask(__name__)
-socketio = SocketIO(app)
+socketio = SocketIO(app, logger=True, engineio_logger=True)
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('ns-interface.html')
+
 
 def on_button_press():
-    socketio.emit('button_press', {}, namespace='/')
+    sleep(5)
+    socketio.emit('press', namespace='/')
 
 
 def listen_for_button():
@@ -20,6 +24,7 @@ def listen_for_button():
         button.wait_for_press()
         on_button_press()
 
+
 if __name__ == '__main__':
     Thread(target=listen_for_button).start()
-    socketio.run(app, host='192.168.0.120', port=5000, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
