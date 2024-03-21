599caf80b4c70e77fbb9805af2ce82e734e893ba
ge54guv <ge54guv@mytum.de>
2024-03-21 12:50:39 +0100

    control panel with threads

--- control_panel.py
+++ control_panel.py
@@ -6,7 +6,7 @@ from time import sleep
 # Initialize the button
 pain_button = Button(27)
 drink_button = Button(17)
-chat_button = Button(17)
+chat_button = Button(2)
 schedule_button = Button(3)
 orientation_button = Button(4)
 
@@ -18,11 +18,10 @@ schedule_process = None
 orientation_process = None
 
 
-
 def start_drink():
     global drink_process
     while True:
-        #drink_button.wait_for_press()
+        drink_button.wait_for_press()
         print("Start the drink.py...")
         drink_process = subprocess.Popen(["python", "drink.py"])
         sleep(2)
@@ -33,11 +32,10 @@ def start_drink():
         sleep(2)
 
 
-
 def start_schedule():
     global schedule_process
     while True:
-        #schedule_button.wait_for_press()
+        schedule_button.wait_for_press()
         print("Start the schedule.py...")
         schedule_process = subprocess.Popen(["python", "schedule.py"])
         sleep(2)
@@ -48,11 +46,10 @@ def start_schedule():
         sleep(2)
 
 
-
 def start_orientation():
     global orientation_process
     while True:
-        #orientation_button.wait_for_press()
+        orientation_button.wait_for_press()
         print("Start the orientation.py...")
         orientation_process = subprocess.Popen(["python", "orientation.py"])
         sleep(2)
@@ -66,7 +63,7 @@ def start_orientation():
 def start_chat():
     global chat_process
     while True:
-        #chat_button.wait_for_press()
+        chat_button.wait_for_press()
         print("Start the chat.py...")
         chat_process = subprocess.Popen(["python", "chat.py"])
         sleep(2)
@@ -80,7 +77,7 @@ def start_chat():
 def start_pain():
     global pain_process
     while True:
-        #pain_button.wait_for_press()
+        pain_button.wait_for_press()
         print("Start the pain.py...")
         pain_process = subprocess.Popen(["python", "pain.py"])
         sleep(2)
@@ -118,21 +115,21 @@ def run_nurse_station():
 # Thread for running the patient_screen.py script
 t_patient_screen = threading.Thread(target=run_patient_screen)
 t_nurse_station = threading.Thread(target=run_nurse_station)
-t_function = threading.Thread(target=run_function)
+#t_function = threading.Thread(target=run_function)
 
-#t_chat = threading.Thread(target=start_chat)
-#t_pain = threading.Thread(target=start_pain)
-# t_drink = threading.Thread(target=start_drink)
-# t_schedule = threading.Thread(target=start_schedule)
-# t_orientation = threading.Thread(target=start_orientation)
+t_chat = threading.Thread(target=start_chat)
+t_pain = threading.Thread(target=start_pain)
+t_drink = threading.Thread(target=start_drink)
+t_schedule = threading.Thread(target=start_schedule)
+t_orientation = threading.Thread(target=start_orientation)
 
 # Start the patient_screen.py script thread
 t_patient_screen.start()
 t_nurse_station.start()
-t_function.start()
+#t_function.start()
 
-#t_chat.start()
-#t_pain.start()
-# t_drink.start()
-# t_schedule.start()
-# t_orientation.start()
+t_chat.start()
+t_pain.start()
+t_drink.start()
+t_schedule.start()
+t_orientation.start()
