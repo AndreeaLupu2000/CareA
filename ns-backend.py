47e9516971e414795d99b73ed7f13e091c713692
ge54guv <ge54guv@mytum.de>
2024-03-21 10:03:34 +0100

    integration of the nurse station

--- ns-backend.py
+++ ns-backend.py
@@ -1,8 +1,14 @@
 from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
 from time import sleep
 from gpiozero import Button
 from threading import Thread
+
+button_chat = Button(2)
+button_pain = Button(4)
+button_drink = Button(3)
+button_schedule = Button(17)
+button_orientation = Button(19)
 
 app = Flask(__name__)
 socketio = SocketIO(app, logger=True, engineio_logger=True)
@@ -13,18 +19,40 @@ def index():
     return render_template('ns-interface.html')
 
 
-def on_button_press():
-    sleep(5)
-    socketio.emit('press', namespace='/')
-
-
 def listen_for_button():
-    button = Button(27)
+    global button_chat
+    global button_pain
+    global button_drink
+    global button_schedule
+    global button_orientation
+
+
     while True:
-        button.wait_for_press()
-        on_button_press()
+        try:
+            if button_chat.is_pressed:
+                print("Chat button pressed")
+                sleep(3)  # Simulate delay
+                socketio.emit('pressed', 'chat', namespace='/')
+            elif button_pain.is_pressed:
+                print("Pain button pressed")
+                sleep(3)  # Simulate delay
+                socketio.emit('pressed', 'pain', namespace='/')
+            elif button_drink.is_pressed:
+                print("Drink button pressed")
+                sleep(3)  # Simulate delay
+                socketio.emit('pressed', 'drink', namespace='/')
+            elif button_schedule.is_pressed:
+                print("Schedule button pressed")
+                sleep(3)  # Simulate delay
+                socketio.emit('pressed', 'schedule', namespace='/')
+            elif button_orientation.is_pressed:
+                print("Orientation button pressed")
+                sleep(3)  # Simulate delay
+                socketio.emit('pressed', 'orientation', namespace='/')
+        except KeyboardInterrupt:
+            break
 
 
 if __name__ == '__main__':
     Thread(target=listen_for_button).start()
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True, allow_unsafe_werkzeug=True)
+    socketio.run(app, host='127.0.0.1', port=5000, debug=False, allow_unsafe_werkzeug=True)
